d19b0a8d1d5c62167fc010adcc7b0993d7583d35
hannah hayward <hannahhayward97@gmail.com>
2021-07-14 16:45:09 -0600

    fixed the errors

--- setup.sql
+++ setup.sql
@@ -1,20 +1,20 @@
 CREATE TABLE IF NOT EXISTS castles (  
     id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
-    update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  'update time',
-    name varchar(255) comment 'Name of Castle'
-    origin VARCHAR (255) comment 'Country of Origin'
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  'update time',
+    name VARCHAR(255) comment 'Name of Castle',
+    origin VARCHAR(255) comment 'Country of Origin'
 ) default charset utf8 comment '';
 CREATE TABLE IF NOT EXISTS knights (  
     id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
     createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
-    update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT  'update time',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update time',
     name varchar(255) comment 'Name of Knight',
     title varchar(255) comment 'Title of Knight',
-    origin VARCHAR (255) comment 'Country of Origin',
+    origin VARCHAR(255) comment 'Country of Origin',
     yearOfBirth INT comment 'Year of Birth',
     yearOfDeath INT comment 'Year of Death',
-    castleId INT  NOT  NULL COMMENT 'FK: Castle',
+    castleId INT NOT NULL COMMENT 'FK: Castle',
 
     FOREIGN KEY (castleId) REFERENCES castles(id) on Delete CASCADE 
 ) default charset utf8 comment '';
